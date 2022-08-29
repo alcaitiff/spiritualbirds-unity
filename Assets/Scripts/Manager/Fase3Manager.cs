62c57e46202efe6cd7dc4e4f584251a1d7abf3f8
Alan Rabello <alan.rabello@serpro.gov.br>
2022-08-29 10:49:59 -0300

    Fase 3 logic

--- Assets/Scripts/Manager/Fase3Manager.cs
+++ Assets/Scripts/Manager/Fase3Manager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class Fase2Manager : Fase1Manager
+public class Fase3Manager : Fase1Manager
 {
     private bool superSelected = false;
     override protected void Start()
@@ -18,9 +18,9 @@ public class Fase2Manager : Fase1Manager
         UI.showSuperSelection();
         
         //only for tests
-        gm.stats[(int)EnemyIndexes.PIDGEON].born=120;
-        bossStats.killed=1;
-        boss=true;
+        //gm.stats[(int)EnemyIndexes.PIDGEON].born=120;
+        //bossStats.killed=1;
+        //boss=true;
     }
 
     override protected void Update(){
@@ -31,7 +31,7 @@ public class Fase2Manager : Fase1Manager
         }
         if(superSelected){
             EnemyStats pidgeonStats = gm.stats[(int)EnemyIndexes.PIDGEON];
-            if(pidgeonStats.born<120){
+            if(pidgeonStats.born<220){
                 enableEnemies();        
             }else if(!boss){
                 enableBoss();
@@ -47,31 +47,31 @@ public class Fase2Manager : Fase1Manager
         if(!pidgeon.active){
             pidgeon.setInterval(4f);
             pidgeon.enable();
-        }else if(!woodpecker.active && pidgeonStats.born>66 && pidgeonStats.born<72){
+        }else if(!woodpecker.active && pidgeonStats.born>126 && pidgeonStats.born<132){
             pidgeon.setInterval(2f);
-        }else if(!woodpecker.active && pidgeonStats.born>72){
+        }else if(!woodpecker.active && pidgeonStats.born>132){
             pidgeon.setInterval(4f);
             woodpecker.setInterval(6f);
             woodpecker.enable();
-        }else if(!hawk.active && pidgeonStats.born>78){
+        }else if(!hawk.active && pidgeonStats.born>138){
             pidgeon.setInterval(5f);
             woodpecker.setInterval(7f);
             hawk.setInterval(6f);
             hawk.enable();
-        }else if(!blueJay.active && pidgeonStats.born>84){
+        }else if(!blueJay.active && pidgeonStats.born>144){
             pidgeon.setInterval(5f);
             woodpecker.setInterval(8f);
             hawk.setInterval(7f);
             blueJay.setInterval(9f);
             blueJay.enable();
-        }else if(!orangeBird.active && pidgeonStats.born>90){
+        }else if(!orangeBird.active && pidgeonStats.born>160){
             pidgeon.setInterval(5f);
             woodpecker.setInterval(8f);
             hawk.setInterval(7f);
             blueJay.setInterval(10f);
             orangeBird.setInterval(4f);
             orangeBird.enable();     
-        }else if(!crow.active && pidgeonStats.born>100){
+        }else if(!crow.active && pidgeonStats.born>200){
             pidgeon.setInterval(5f);
             woodpecker.setInterval(9f);
             hawk.setInterval(8f);
